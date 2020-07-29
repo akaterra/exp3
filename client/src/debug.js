411670770413f90517f8f36adec516f7cd798945
nikolay.klyuchev <nikolay.klyuchev@getpackage.com>
2020-07-29 18:40:05 +0300

    ?

--- client/src/debug.js
+++ client/src/debug.js
@@ -1,5 +1,9 @@
-export class Debug {
+import { SubjectWithCache } from './subject_with_cache';
+
+export class Debug extends SubjectWithCache {
   constructor() {
+    super();
+
     this._flowNodes = new Map();
     this._root = new DebugNode();
   }
@@ -7,6 +11,8 @@ export class Debug {
   setMode(flow, mode) {
     if (this._flowNodes.has(flow)) {
       this._flowNodes.get(flow).setMode(mode);
+
+      this.next({ action: null, data: this.toJSON() });
     }
 
     return this;
@@ -16,15 +22,19 @@ export class Debug {
     let rootNode = !parent ? this._root : this._flowNodes.get(parent);
 
     if (!rootNode) {
-      rootNode = new DebugNode(parent);
+      this._root = rootNode = new DebugNode(parent);
 
       this._flowNodes.set(parent, rootNode);
+
+      this.next({ action: null, data: this.toJSON() });
     }
 
     if (!rootNode.has(flow)) {
       let flowNode = new DebugNode(flow);
 
-      rootNode.add(flowNode);console.log(this);
+      rootNode.add(flowNode);
+
+      this.next({ action: null, data: this.toJSON() });
     }
 
     return this;
@@ -33,43 +43,70 @@ export class Debug {
   remove(flow) {
     if (this._flowNodes.has(flow)) {
       this._flowNodes.delete(this._flowNodes.get(flow).clear());
+
+      this.next({ action: null, data: this.toJSON() });
     }
 
     return this;
+  }
+
+  toJSON() {
+    return {
+      root: this._root.toJSON(),
+    };
   }
 }
 
 export class DebugNode {
   constructor(flow) {
-    this.flow = flow;
-    this.nodes = new Set();
+    this._flow = flow;
+    this._nodes = new Set();
   }
 
   setMode(mode) {
-    this.mode = mode;
+    this._mode = mode;
   }
 
   clear() {
-    for (const node of this.nodes) {
+    for (const node of this._nodes) {
       node.clear();
     }
 
-    this.flow = this.nodes.clear();
+    this._flow = this._nodes.clear();
 
     return this;
   }
 
   has(flow) {
-    return this.nodes.has(flow);
+    return this._nodes.has(flow);
   }
 
   add(flow) {
-    this.nodes.add(flow);
+    this._nodes.add(flow);
   }
 
   remove(flow) {
-    this.nodes.delete(flow);
+    this._nodes.delete(flow);
+  }
+
+  toJSON() {
+    const nodes = {};
+
+    for (const node of this._nodes.entries()) {
+      nodes[Object.getPrototypeOf(node[0]._flow).constructor.name] = node[1].toJSON();
+    }
+
+    return {
+      mode: this._mode,
+      nodes,
+    };
   }
 }
 
 export const debug = new Debug();
+
+// debug.add('123');
+// debug.add('333', '123');
+// debug.add('444', '123');
+
+// console.log(debug.toJSON());
--- client/src/index.js
+++ client/src/index.js
@@ -7,14 +7,36 @@ import './json-inspector.css';
 import './index.css';
 import './index.sass';
 import { connectionManager } from './api';
+import { debug } from './debug';
+import { Source } from './atom';
 import { default as MainFlow } from './entity/main';
 
 applyIsArrayPatch();
+
+const style = {
+  overlap: {
+    backgroundColor: '#ff000010',
+    position: 'fixed',
+    right: 0,
+    top: 0,
+    width: '300px',
+    zIndex: 2000,
+  },
+}
+
+function Debug(props) {
+  return <pre style={ style.overlap }>{ JSON.stringify(props.json, undefined, 2) }</pre>;
+}
 
 const mainFlow = new MainFlow(connectionManager);
 mainFlow.run();
 
 ReactDOM.render(
-  <MainFlow.Component flow={ mainFlow }></MainFlow.Component>,
+  <React.Fragment> 
+    <MainFlow.Component flow={ mainFlow }/>
+    <Source source={ debug } prop='json'>
+      <Debug/>
+    </Source>
+  </React.Fragment>,
   document.getElementById('container'),
 )
