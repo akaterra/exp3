0bc58b19d6cc04cc096d92e02f9c123a61b7dfe8
nikolay.klyuchev <nikolay.klyuchev@getpackage.com>
2020-08-11 17:52:56 +0300

    ?

--- client/src/util/parse.js
+++ client/src/util/parse.js
@@ -1,11 +1,152 @@
-const stopSymbols1 = ' =!&|';
-const stopSymbols2 = ' ,%()';
+const STOP_SYM = ' ,()=&&|<>!';
+const STOP_LEX = [
+  ' ',
+  ',',
+  '(',
+  ')',
+  '>=',
+  '<=',
+  '>',
+  '<',
+  '=',  // eq
+  '!=', // not eq
+  '%',  // like
+  '!%', // not like
+  '&',  // and
+  '!&', // not and
+  '|',  // or
+  '!|', // not or
+]
 
-function parse(str) {
-  let ops = [];
+function parseLexVal(lex) {
+  if (lex[1] === 'q') {
+    return lex[0];
+  }
 
-  for (const lex of split(str)) {
+  if (lex[0].toLowerCase() === 'true') {
+    return true;
+  }
 
+  if (lex[0].toLowerCase() === 'false') {
+    return false;
+  }
+
+  if (lex[0].toLowerCase() === 'null') {
+    return null;
+  }
+
+  return parseFloat(lex[0]);
+}
+
+function parse(str, ops) {
+  if (!ops) {
+    ops = [];
+  }
+
+  ops.push({});
+
+  let lex;
+  let nxt = fetch(split(str));
+
+  while (lex = nxt(), lex !== undefined) {
+    if (isC(lex)) {
+      return ops; // unexpected control char
+    }
+
+    let key = lex[0];
+
+    if (!ops[ops.length - 1][key]) {
+      ops[ops.length - 1][key] = {};
+    }
+
+    lex = nxt();
+
+    if (isC(lex)) {
+      switch (lex[0]) {
+        case '>':
+          lex = nxt();
+
+          if (lex === undefined || isC(lex)) {
+            return ops;
+          }
+
+          ops[ops.length - 1][key].gt = parseLexVal(lex);
+          break;
+        case '<':
+          lex = nxt();
+
+          if (lex === undefined || isC(lex)) {
+            return ops;
+          }
+
+          ops[ops.length - 1][key].lt = parseLexVal(lex);
+          break;
+        case '>=':
+          lex = nxt();
+
+          if (lex === undefined || isC(lex)) {
+            return ops;
+          }
+
+          ops[ops.length - 1][key].gte = parseLexVal(lex);
+          break;
+        case '<=':
+          lex = nxt();
+
+          if (lex === undefined || isC(lex)) {
+            return ops;
+          }
+
+          ops[ops.length - 1][key].lte = parseLexVal(lex);
+          break;
+        case '=':
+          lex = nxt();
+
+          if (lex === undefined || isC(lex)) {
+            return ops;
+          }
+
+          ops[ops.length - 1][key].eq = parseLexVal(lex);
+          break;
+        case '!=':
+          lex = nxt();
+
+          if (lex === undefined || isC(lex)) {
+            return ops;
+          }
+
+          ops[ops.length - 1][key].ne = parseLexVal(lex);
+          break;
+        case '&':
+          ops[ops.length - 1][key].eq = true;
+          break;
+        case '|':
+          ops.push({});
+          break;
+      }
+    } else {
+      ops[ops.length - 1][key].eq = true;
+
+      nxt(-2);
+    }
+  }
+
+  return ops;
+}
+
+function isC(lex) {
+  return lex && lex[1] === 'c';
+}
+
+function fetch(arr) {
+  let ind = 0;
+
+  return function (off) {
+    if (off) {
+      ind += off;
+    }
+
+    return arr[ind ++];
   }
 }
 
@@ -37,10 +178,7 @@ function split(str) {
         continue;
       }
 
-      const isStopSymbol1 = stopSymbols1.indexOf(sym) !== -1;
-      const isStopSymbol2 = stopSymbols2.indexOf(sym) !== -1;
-
-      if (!isStopSymbol1 && !isStopSymbol2) {
+      if (STOP_SYM.indexOf(sym) === -1) {
         cur += sym;
       } else {
         if (cur) {
@@ -52,14 +190,24 @@ function split(str) {
         if (sym === ' ') {
           continue;
         } else {
-          if (isStopSymbol1 && tok.length && tok[tok.length - 1][1] === 'c') {
-            tok[tok.length - 1][0] += sym;
+          if (tok.length) {
+            const stopLex = STOP_LEX.findIndex(_ => _ === tok[tok.length - 1][0] + sym);
+
+            if (stopLex !== -1) {
+              tok[tok.length - 1][0] += sym;
+            } else {
+              tok.push([sym, 'c']);
+            }
           } else {
             tok.push([sym, 'c']);
           }
         }
       }
     }
+  }
+
+  if (cur) {
+    tok.push([cur, 's']);
   }
 
   return tok;
@@ -71,4 +219,5 @@ module.exports = {
 };
 
 
-console.log(split('"     " a & ngf ! = 7 & (j="7 =^&\\"hhgd")'));
+// console.log(split('"     " a & ngf ! = 7 & (j="7 =^&\\"hhgd")'));
+console.log(parse('x a & c >= "bcvbxvcbx  f" & d & e'));
